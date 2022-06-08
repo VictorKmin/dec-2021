1199f9d3701230d53ac0877724e459e0a5768a0d
VictorKmin <kminvictor@gmail.com>
2022-06-08 21:36:12 +0300

    EXPRESS and code style basic guide

--- app.js
+++ app.js
@@ -1,12 +1,69 @@
-const { createUser } = require('./services/user.service');
-require('./services/file.service');
+const express = require('express');
+const axios = require('axios');
+const fs = require('fs/promises');
+const users = require('./dataBase/users');
 
-const user = createUser('Viktor', 26);
+const app = express();
 
-console.log(user);
+app.get('/', async (req, res) => {
+  console.log(req);
 
-user.sayHello();
+  const resp = await axios.get('https://jsonplaceholder.typicode.com/users');
 
-// KISS
-// YAGNI
-// SOLID
+  res.status(resp.status).json(resp.data);
+});
+
+app.get('/users', async (req, res) => {
+  let buffer = await fs.readFile('asdadasd');
+  res.json(buffer.toString());
+});
+
+// CREATE
+app.post('/users/:userName/create', (req, res) => {
+  users.push({
+    name: req.params.userName,
+    age: Math.random()*100
+  });
+
+  res.status(201).json('Users was created');
+});
+
+app.delete('/users/:usersId', (req, res) => {
+  users.push({
+    name: 'TEST',
+    age: Math.random()*100
+  });
+
+  res.status(201).json('Users was created')
+});
+app.put('/users/:usersId', (req, res) => {
+  users.push({
+    name: 'TEST',
+    age: Math.random()*100
+  });
+
+  res.status(201).json('Users was created')
+});
+
+app.get('/users/:userId', (req, res) => {
+  const userIndex = +req.params.userId;
+
+  if (isNaN(userIndex) || userIndex < 0) {
+    res.status(400).json('Please enter valid ID');
+    return;
+  }
+
+  const user = users[userIndex];
+
+  if (!user) {
+    res.status(404).json(`Use with ID ${userIndex} is not found`);
+    return;
+  }
+
+  res.json(user);
+});
+
+
+app.listen(5000, () => {
+  console.log('Server listen 5000')
+});
---
+++ dataBase/users.js
@@ -0,0 +1,8 @@
+module.exports = [
+  { name: 'Viktor' },
+  { name: 'Karina' },
+  { name: 'Dima' },
+  { name: 'Nastya' },
+  { name: 'Igor' },
+  { name: 'Annya' },
+];
